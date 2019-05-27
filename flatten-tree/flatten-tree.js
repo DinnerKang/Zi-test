db9f6c3c3cb27b7f6a501f29a9592840200860ae
DinnerKang <jenhyuk0318@gmail.com>
2019-05-27 18:51:21 +0900

    기능 완료 리펙토링 대기

--- flatten-tree/flatten-tree.js
+++ flatten-tree/flatten-tree.js
@@ -1,28 +1,42 @@
 
+let categories;
 
-const categories = require('./categories.json');
+const getData = async () => {
+    const res = await axios.get('./categories.json');
+    categories = res.data;
+    getTreeView(categories);
+}
+getData();
 
-console.log(categories);
 
-const getTree = (obj) =>{
+const getTreeView = (obj) => {
     let result = [];
-    
+
     const firstKey = Object.keys(obj);
-    console.log(firstKey);
-    for(let i=0, len_1=firstKey.length; i<len_1; i++){
-        const secondCategories = obj[firstKey[i]];
+
+    for(let a=0, lengthOfFirst=firstKey.length; a<lengthOfFirst; a++){
+        let str = firstKey[a];
+        const secondCategories = obj[firstKey[a]];
         const secondKey = Object.keys(secondCategories);
 
-        for(let j=0,len_2=secondKey.length; j<len_2;j++){
-            let str = "";
-            const thirdCategories = secondCategories[secondKey[j]];
-            const thirdNode =  Object.keys(thirdCategories);
-            str += firstKey[i] + ' > ' + secondKey[i] + ' > ' + thirdNode[i];
-            result.push(str);
+        for(let i=0, lengthOfSecond=secondKey.length;i<lengthOfSecond; i++){
+            if (secondKey[i]) {
+                let str_2 = str + ' > ' + secondKey[i];
+    
+                const thirdCategories = secondCategories[secondKey[i]];
+                const thirdKey = Object.keys(thirdCategories);
+                if(!thirdKey[i])  result.push(str_2);
+                for(let j=0, lengthOfThird=thirdKey.length; j<lengthOfThird; j++){
+                    let str_3 = str_2 + ' > ' + thirdKey[j];
+                    result.push(str_3);
+                }
+            }
         }
     }
 
+    
+    
+    
+    
     console.log(result);
 }
-
-getTree(categories);
