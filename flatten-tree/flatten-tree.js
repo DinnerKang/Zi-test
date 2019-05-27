c9d11f173e8522fcdd065f1ef2af96d6610b669a
DinnerKang <jenhyuk0318@gmail.com>
2019-05-27 20:27:55 +0900

    flatten-tree 리팩토링 - axios 사용, 에러처리, 변수 선언, 오류 수정

--- flatten-tree/flatten-tree.js
+++ flatten-tree/flatten-tree.js
@@ -1,42 +1,53 @@
 
-let categories;
 
-const getData = async () => {
-    const res = await axios.get('./categories.json');
-    categories = res.data;
-    getTreeView(categories);
+
+// 테스트 시 데이터 경로 추가
+getData('./categories.json');
+
+
+async function getData(path){
+    try{
+        const res = await axios.get(path);
+        getTreeView(res.data);
+    }catch(e){
+        alert('서버에서 실행시켜야 합니다.');
+    }
 }
-getData();
 
+const getTreeView = (categories) => {
 
-const getTreeView = (obj) => {
-    let result = [];
+    const result = [];
+    const firstKey = Object.keys(categories);
+    let str, str_2, str_3;
+    let secondCategories, thirdCategories;
+    let secondKey, thirdKey;
 
-    const firstKey = Object.keys(obj);
 
     for(let a=0, lengthOfFirst=firstKey.length; a<lengthOfFirst; a++){
-        let str = firstKey[a];
-        const secondCategories = obj[firstKey[a]];
-        const secondKey = Object.keys(secondCategories);
+        str = firstKey[a];
+        secondCategories = categories[firstKey[a]];
+        secondKey = Object.keys(secondCategories);
+
+
+        if(!secondKey[0])  result.push(str);
+
 
         for(let i=0, lengthOfSecond=secondKey.length;i<lengthOfSecond; i++){
-            if (secondKey[i]) {
-                let str_2 = str + ' > ' + secondKey[i];
-    
-                const thirdCategories = secondCategories[secondKey[i]];
-                const thirdKey = Object.keys(thirdCategories);
-                if(!thirdKey[i])  result.push(str_2);
-                for(let j=0, lengthOfThird=thirdKey.length; j<lengthOfThird; j++){
-                    let str_3 = str_2 + ' > ' + thirdKey[j];
-                    result.push(str_3);
-                }
+            str_2 = str + ' > ' + secondKey[i];
+            thirdCategories = secondCategories[secondKey[i]];
+            thirdKey = Object.keys(thirdCategories);
+
+
+            if(!thirdKey[0])  result.push(str_2);
+
+            
+            for(let j=0, lengthOfThird=thirdKey.length; j<lengthOfThird; j++){
+                str_3 = str_2 + ' > ' + thirdKey[j];
+                result.push(str_3);
             }
         }
     }
-
-    
-    
-    
-    
-    console.log(result);
+    console.log('결과 : ',result);
+    return result;
 }
+
