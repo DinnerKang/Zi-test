5718fd097771078e531815334e6a033a5a05dbcc
DinnerKang <jenhyuk0318@gmail.com>
2019-05-30 20:51:17 +0900

    flatten-tree 리팩토링 : 변수 이름 수정 , 결과에 필요없는 코드 삭제

--- flatten-tree/flatten-tree.js
+++ flatten-tree/flatten-tree.js
@@ -1,11 +1,11 @@
 
 
 
-expect(getData('./categories.json'), 130);
-expect(getData('./test-no-oneNode.json'), 129);
-expect(getData('./test1.json'), 126);
+expect(jsonPath('./categories.json'), 130);
+expect(jsonPath('./test-no-oneNode.json'), 129);
+expect(jsonPath('./test1.json'), 126);
 
-// 테스트 함수
+// 결과의 총 노드 개수와 알고있는 노드의 총 개수 비교
 async function expect(result, expected) {
     const res = await result;
     if( res.length != expected ){
@@ -15,58 +15,43 @@ async function expect(result, expected) {
     }
 }
 
-
-
-async function getData(path){
+async function jsonPath(path){
     try{
         const res = await axios.get(path);
-        return getTreeView(res.data);
+        return getTree(res.data);
     }catch(e){
         console.log(e);
     }
 }
 
-const getTreeView = (categories) => {
+const getTree = (categories) => {
 
     const result = [];
     const firstKey = Object.keys(categories);
     let str, str_2, str_3;
     let secondCategories, thirdCategories;
     let secondKey, thirdKey;
-    let oneNode = 0, twoNode = 0, threeNode =0;
 
     for(let a=0, lengthOfFirst=firstKey.length; a<lengthOfFirst; a++){
         str = firstKey[a];
         secondCategories = categories[firstKey[a]];
         secondKey = Object.keys(secondCategories);
         
-
-        if(!secondKey[0])  {
-            result.push(str);
-            oneNode += 1;
-        }
-
+        if(!secondKey[0]) result.push(str);
+        
         for(let i=0, lengthOfSecond=secondKey.length;i<lengthOfSecond; i++){
             str_2 = str + ' > ' + secondKey[i];
             thirdCategories = secondCategories[secondKey[i]];
             thirdKey = Object.keys(thirdCategories);
 
-
-            if(!thirdKey[0])  {
-                result.push(str_2);
-                twoNode += 1;
-            }
+            if(!thirdKey[0]) result.push(str_2);
             
             for(let j=0, lengthOfThird=thirdKey.length; j<lengthOfThird; j++){
                 str_3 = str_2 + ' > ' + thirdKey[j];
                 result.push(str_3);
-                threeNode += 1;
             }
         }
     }
-    console.log('첫번째 노드 갯수 :',oneNode);
-    console.log('두번째 노드 갯수 :',twoNode);
-    console.log('세번째 노드 갯수 :',threeNode);
     console.log('결과 : ',result);
     return result;
 }
