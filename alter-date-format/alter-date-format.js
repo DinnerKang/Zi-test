d954dbd647ed9459812cf623654948828f6ac6e9
DinnerKang <jenhyuk0318@gmail.com>
2019-05-31 18:02:08 +0900

    에러검출 코드 추가 및 변수 리펙토링

--- alter-date-format/alter-date-format.js
+++ alter-date-format/alter-date-format.js
@@ -10,9 +10,14 @@ expect(timeIncrease('AM 05:00:00', 72000)).toBe('01:00:00');
 expect(timeIncrease('AM 05:00:00', 108000)).toBe('11:00:00');
 expect(timeIncrease('AM 11:00:00', 90000)).toBe('12:00:00');
 expect(timeIncrease('PM 11:00:00', 90000)).toBe('00:00:00');
+expect(timeIncrease('PM 123', 90000)).toBe('00:00:00');
+expect(timeIncrease('PM 11:00:00', 'PM 11:00:00')).toBe('00:00:00');
 
-function timeIncrease(nowDate, n){
+function timeIncrease(nowDate, n){ 
     console.log('들어온 시간 :',nowDate);
+    if(nowDate.split('').length !== 11 ||typeof(n) !== 'number'){
+        throw new Error(`입력값이 이상합니다 nowDate : ${nowDate} n : ${n}`);
+    }
     let result;
     const day = nowDate.slice(0,2);
     const time = nowDate.substr(3).split(':');
@@ -20,12 +25,12 @@ function timeIncrease(nowDate, n){
     const getMinutes = time[1] * 60;
     const getSeconds = Number(time[2]);
 
-    const getAllTime = getHours + getMinutes + getSeconds + n;
+    const timeSum = getHours + getMinutes + getSeconds + n;
 
 
-    var hours   = Math.floor(getAllTime / 3600);
-    var minutes = Math.floor((getAllTime - (hours * 3600)) / 60);
-    var seconds = getAllTime - (hours * 3600) - (minutes * 60);
+    let hours   = Math.floor(timeSum / 3600);
+    let minutes = Math.floor((timeSum - (hours * 3600)) / 60);
+    let seconds = timeSum - (hours * 3600) - (minutes * 60);
 
     if(day == 'AM' && hours == 12) hours = hours - 12;
     if(day == 'PM') hours = hours + 12;
